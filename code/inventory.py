486570120d8ea195f71beaea62d68e7f9f77182f
magnetarz <kwardrip105@gmail.com>
2023-10-12 19:58:31 -0700

    loading testbed and connectiong to devices

--- code/inventory.py
+++ code/inventory.py
@@ -5,16 +5,25 @@ Goal:
     -Create a CSV inventory file
     device name, software version, uptime, serial number
     """
-
+from pyats.topology.loader import load
 #Script entry point
 if __name__ == "__main__":
+    import argparse
+    
     print("creatin a network inventory script.")
 
     #Load pyATS testbed into script
+    parser = argparse.ArgumentParser(
+        description='Generate network inventory report')
+    parser.add_argument('testbed', type=str,
+                        help='pyATS Testbed File')
+    args = parser.parse_args()
 
 
+    #Create pyATS testbed object
+    testbed = load(args.testbed)
     #Connect to network devices
-
+    testbed.connect()
 
 
     #Run commands to gather output from devices
