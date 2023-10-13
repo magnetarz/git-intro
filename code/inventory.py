64f9b7ac932bcd2fd8421982a55c56bcfbf32800
magnetarz <kwardrip105@gmail.com>
2023-10-12 19:46:02 -0700

    created inventory script and made it executable

---
+++ code/inventory.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+"""
+A basic network inventory generation script.
+Goal:
+    -Create a CSV inventory file
+    device name, software version, uptime, serial number
+    """
+
+#Script entry point
+if __name__ == "__main__":
+    print("creatin a network inventory script.")
+
+    #Load pyATS testbed into script
+
+
+    #Connect to network devices
+
+
+
+    #Run commands to gather output from devices
+
+
+
+
+    #Build inventory report data structure
+
+
+
+
+    #Generate CSV file of data
